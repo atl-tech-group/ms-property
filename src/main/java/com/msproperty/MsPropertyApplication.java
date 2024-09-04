7f08bf679f4ecd99e6c07f833521fca1c23fc2f4
Nihad <nihadamirov98@gmail.com>
2024-09-04 10:26:51 +0400

    init file

---
+++ src/main/java/com/msproperty/MsPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.msproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPropertyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msproperty/MsPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
