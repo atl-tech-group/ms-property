39d1bdd647d48ae53b6de902bf01c6ba121ea639
thehasanovv <hasanov.e95@gmail.com>
2024-09-23 12:16:43 +0400

    Global exception handler - Not found exception

--- src/main/java/com/msproperty/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/msproperty/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.msproperty.model.response.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
