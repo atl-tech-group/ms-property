67592efc9a2179ac6601c59db94502746a511972
thehasanovv <hasanov.e95@gmail.com>
2024-09-23 12:58:09 +0400

    MethodArgumentNotValidException & Not FoundException added to GlobalExceptionHander class

--- src/main/java/com/msproperty/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/msproperty/exceptions/GlobalExceptionHandler.java
@@ -3,9 +3,17 @@ package com.msproperty.exceptions;
 import com.msproperty.model.response.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
@@ -18,6 +26,26 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(NOT_FOUND)
                 .body(ExceptionResponse.builder()
                         .message(ex.getMessage())
+                        .statusCode(BAD_REQUEST.value())
+                        .timestamp(LocalDateTime.now())
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handle(MethodArgumentNotValidException ex) {
+        log.error("MethodArgumentNotValidException : " + ex);
+
+        Map<String, String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        return ResponseEntity.status(BAD_REQUEST)
+                .body(ExceptionResponse.builder()
+                        .message("Field validation failed")
+                        .statusCode(BAD_REQUEST.value())
+                        .errors(errors)
+                        .timestamp(LocalDateTime.now())
                         .build());
     }
 }
--- src/main/java/com/msproperty/model/response/ExceptionResponse.java
+++ src/main/java/com/msproperty/model/response/ExceptionResponse.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static lombok.AccessLevel.PRIVATE;
 
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
 public class ExceptionResponse {
-    private String message;
+    String message;
+    Integer statusCode;
+    String error;
+    Map<String, String> errors;
+    LocalDateTime timestamp;
 }
